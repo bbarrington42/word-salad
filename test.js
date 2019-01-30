4ce9c969afda3dfd6c015f1eb4a2a992ea14ca0c
Bill Barrington <bill.barrington@candidpartners.com>
2019-01-30 09:29:21 -0500

    Created a separate function to facilitate testing.

--- test.js
+++ test.js
@@ -2,3 +2,43 @@
 
 const assert = require('assert');
 const util = require('./lib/util');
+
+const templates = [
+    "Noun: ${noun}, Verb: ${verb}, Adjective: ${adjective}, Adverb: ${adverb}"
+];
+
+const substitutions = [
+    {
+        partOfSpeech: "adjective",
+        values: [
+            "adjective"
+        ]
+    },
+    {
+        partOfSpeech: "adverb",
+        values: [
+            "adverb"
+        ]
+    },
+    {
+        partOfSpeech: "noun",
+        values: [
+            "noun"
+        ]
+    },
+    {
+        partOfSpeech: "verb",
+        values: [
+            "verb"
+        ]
+    }
+];
+
+const expected = "Noun: noun, Verb: verb, Adjective: adjective, Adverb: adverb";
+
+it('correctly substitutes parts of speech', () => {
+    const actual = util.makeSubs(templates, substitutions);
+
+    assert.deepStrictEqual(actual, expected);
+});
+
